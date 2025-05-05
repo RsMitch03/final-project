07e2c0b8cbfee45e02b8dcf1eb2881b8558533cc
rsmit_lthmjs <rsmitch2003@gmail.com>
2025-05-05 17:06:56 -0500

    Displaying text

--- src/project.py
+++ src/project.py
@@ -6,6 +6,8 @@ def resize_fight():
     for frame in range(amount):
         with Image.open(f"OGFight_scene\\rogue_fight_{frame+1}.jpg") as img:
             size = (int(img.width//10), int(img.height//10))
+            if frame == 13:
+                size = (int(img.width//3.5), int(img.height//3.5))
             new_size = img.resize(size)
             new_size.save(f"Resized_scene\\rogue_fight_{frame+1}.jpg")
 def reformat_image():
@@ -22,6 +24,7 @@ def main():
         draw.text((0, 0), score)
         img.show()"""
     pygame.init()
+    pygame.freetype.init()
     scene = 1
     resize_fight()
     reformat_image()
@@ -33,6 +36,9 @@ def main():
     screen = pygame.display.set_mode(resolution)
     screen.fill(pygame.Color(255, 255, 255))
     screen.blit(display, (0, 0))
+    font = pygame.freetype.SysFont('ariel', (24))
+    text, rect = font.render(f'{score}', (0, 0, 0))
+    screen.blit(text, (15, 15))
     pygame.display.flip()
     running = True
     while running:
@@ -44,6 +50,7 @@ def main():
                     draw.text((0, 0), score)
             if event.type == pygame.KEYDOWN and pygame.K_SPACE:
                 display = pygame.image.load(f"Final_fight_scene\\rogue_fight_{scene+1}.png")
+                screen.fill(pygame.Color(255, 255, 255))
                 screen.blit(display, (0, 0))
                 scene += 1
             if event.type == pygame.QUIT:
