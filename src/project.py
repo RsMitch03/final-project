ac2e6857be1a9bd66e73264e05b67be038d33e27
rsmit_lthmjs <rsmitch2003@gmail.com>
2025-05-09 20:41:50 -0500

    making the sound work

--- src/project.py
+++ src/project.py
@@ -16,7 +16,19 @@ def reformat_image():
           with Image.open(f"Resized_scene\\rogue_fight_{frame+1}.jpg") as img:
               img.save(f"Final_fight_scene\\rogue_fight_{frame+1}.png")
 
-# def music():
+def sliceSound():
+    pygame.mixer.init()
+    slice = pygame.mixer.Sound("Slice.wav")
+    pygame.mixer.Sound.set_volume(slice, .90)
+    pygame.mixer.Sound.play(slice)
+    
+
+def music():
+    pygame.mixer.init()
+    music = pygame.mixer.Sound("Music.wav")
+    pygame.mixer.Sound.set_volume(music, .75)
+    pygame.mixer.Sound.play(music, -1)
+    
 
 def main():
     """with Image.open(pic) as img:
@@ -24,6 +36,7 @@ def main():
         draw.text((0, 0), score)
         img.show()"""
     pygame.init()
+    music()
     pygame.freetype.init()
     scene = 1
     resize_fight()
@@ -51,11 +64,11 @@ def main():
                 scene += 1
             if event.type == pygame.MOUSEBUTTONDOWN:
                 hitCount += 1
+                sliceSound()
                 screen.fill(pygame.Color(255, 255, 255))
                 screen.blit(display, (0, 0))
                 text, rect = font.render(f'Hit Count: {hitCount}', (0, 0, 0))
                 screen.blit(text, (15, 15))
-                print(hitCount)
             if event.type == pygame.QUIT:
                 running = False
         pygame.display.update()
